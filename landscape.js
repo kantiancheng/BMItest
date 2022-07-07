3c66469258effad6f39f1382f726b9f4e6c811e7
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-07-06 21:09:46 -0700

    Update landscape.js

--- landscape.js
+++ landscape.js
@@ -1,9 +1,19 @@
-function hengshuping() {
-    if (window.orientation == 180 || window.orientation == 0) {
-        document.getElementById("landscape").hidden = false;
+function hengshuping(typ) {
+    if (typ == '提示横屏') {
+        if (window.orientation == 180 || window.orientation == 0) {
+            document.getElementById("landscape").hidden = false;
+        }
+        if (window.orientation == 90 || window.orientation == -90) {
+            document.getElementById("landscape").hidden = true;
+        }
     }
-    if (window.orientation == 90 || window.orientation == -90) {
-        document.getElementById("landscape").hidden = true;
+    if (typ == '提示竖屏') {
+        if (window.orientation == 180 || window.orientation == 0) {
+            document.getElementById("landscape").hidden = true;
+        }
+        if (window.orientation == 90 || window.orientation == -90) {
+            document.getElementById("landscape").hidden = false;
+        }
     }
 }
 window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
